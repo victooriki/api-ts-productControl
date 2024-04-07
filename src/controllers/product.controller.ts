190c3532d22730f11e14e6c7013a90a1b85b9b42
Victor Iki <victooriki@gmail.comm>
2024-04-07 20:29:32 -0300

    bodyparse set

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,16 +1,39 @@
-import { Request, Response} from 'express'
+import { Request, response, Response} from 'express'
 import AppDataSource from '../connection';
 import { Product } from '../entities/product.entity';
+import { Repository } from 'typeorm';
 
 class ProductController {
-    async findAll(request: Request, response: Response): Promise<Response> {
-        const productRepository = AppDataSource.getRepository(Product);
+    private productRepository: Repository<Product>
 
-        const products = await productRepository.find();
+    constructor() {
+        this.productRepository = AppDataSource.getRepository(Product)
+    }
+
+    async findAll(request: Request, response: Response): Promise<Response> {
+        const productRepository = AppDataSource.getRepository(Product)
+        const products = await productRepository.find()
 
         return response.status(200).send({
             data: products
-        });
+        })
+    }
+
+    async create(request: Request, response: Response): Promise<Response> {
+        const {nome, peso, descricao} = request.body
+
+        const productRepository = AppDataSource.getRepository(Product)
+
+        const product = new Product
+        product.nome = nome
+        product.peso = peso
+        product.descricao = descricao
+
+        const dbProduct = await productRepository.save(product)
+
+        return response.status(201).send({
+            data: dbProduct
+        })
     }
 }
 
